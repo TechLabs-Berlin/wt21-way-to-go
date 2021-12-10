616454d3679ca4ff078d7aae41d54a10599d8e2c
Leonor <leonorcascais@hotmail.com>
2021-12-10 21:57:58 +0100

    add map

--- src/index.js
+++ src/index.js
@@ -1,9 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '600px',
+  height: '600px'
+};
+
+const center = {
+  lat: 52.520008,
+  lng: 13.404954
+};
 
 const App = function () {
-    return <div>WayToGo!</div>;
+    return <div>WayToGo
+        <Map></Map>
+    </div>
 };
+
+function Map() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "Key"
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+  ) : <></>
+}
+
 
 ReactDOM.render(
     <App />,
