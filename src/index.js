a408f127b6051f0de5a508769c9c181daed17cb3
Leonor <leonorcascais@hotmail.com>
2021-12-10 19:19:53 +0100

    display 1st content with funcional component

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = function () {
+    return <div>WayToGo!</div>;
+};
+
+ReactDOM.render(
+    <App />,
+	document.querySelector('#root')
+)
