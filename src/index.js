d1a1f7d676066457df3b171ec1f5008561a22738
Leonor <leonorcascais@hotmail.com>
2021-12-15 10:06:10 +0000

    clean map code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 const containerStyle = {
-  width: '600px',
+  width: '1000px',
   height: '600px'
 };
 
@@ -24,32 +24,15 @@ function Map() {
     googleMapsApiKey: "Key"
   })
 
-  const [map, setMap] = React.useState(null)
-
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map)
-  }, [])
-
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-
   return isLoaded ? (
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
         zoom={10}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
       >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
       </GoogleMap>
   ) : <></>
 }
-
 
 ReactDOM.render(
     <App />,
