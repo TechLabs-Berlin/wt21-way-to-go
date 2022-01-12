0f5086f7919f2b8ed726497434e5cd89a54e3b50
StefanieSenger <stefanie.senger@posteo.de>
2022-01-12 02:09:34 +0100

    finished parsing routes data (first bulk)

--- cleaning_data/parsing_routes_data.py
+++ cleaning_data/parsing_routes_data.py
@@ -2,28 +2,35 @@ import os
 import pandas as pd
 import gpxpy
 
-#print(os.getcwd()) # for finding out where you're at so you can modify your paths if needed
-indir = r'routes_data_to_parse'
-outdir = r'parsed_routes_data'
+#defining working directories, please replace filepaths
+os.chdir(r'[substitude your path]\WayToGo\wt21-way-to-go\cleaning_data') #setting path back to the original starting path before re-running code
+indir = r'[substitude your path]\WayToGo\wt21-way-to-go\cleaning_data\routes_data_to_parse'
+outdir = r'[substitude your path]\WayToGo\wt21-way-to-go\cleaning_data\parsed_routes_data'
+os.chdir(r'[substitude your path]\WayToGo\wt21-way-to-go\cleaning_data\routes_data_to_parse')
 
-#function for reading routes from all .gpx files into a list
+#function for reading waypoints from a gps file into a list
 def parsegpx(f):
     list_of_routes_dicts = []
     with open (f, 'r') as gpx_file:
-        gpx = gpxpy.parse(gpx_file)
-        for track in gpx.tracks:
-            for segment in track.segments:
-                for point in segment.points:
-                    #lat_lgt_tuple = tuple(('({0},{1})'.format(point.latitude, point.longitude)).strip('()').split(',')) #that would make it into tuples of lat and lgt (might be more useful?)
-                    dict = {'latitude' : point.latitude, 'longitude' : point.longitude}
-                    list_of_routes_dicts.append(dict)
+        try:
+            gpx = gpxpy.parse(gpx_file)
+            for track in gpx.tracks:
+                for segment in track.segments:
+                    for point in segment.points:
+                        lat_lgt_tuple = tuple(('({0},{1})'.format(point.latitude, point.longitude)).strip('()').split(','))
+                        dict = ({'lat_lgt': lat_lgt_tuple, 'latitude' : point.latitude, 'longitude' : point.longitude})
+                        list_of_routes_dicts.append(dict)
+        except:
+            pass
     return list_of_routes_dicts
 
 #parsing all the .gpx files into a pandas DataFrame
 files = os.listdir(indir)
-df = pd.concat([pd.DataFrame(parsegpx(f)) for f in files], keys = files) #error message: first file with 6 instead of 5 digits in it's name not recognised
-#df.head()
+df = pd.concat([pd.DataFrame(parsegpx(f)) for f in files], keys = files)
 
 #writing the data into a csv file
 os.chdir(outdir)
 df.to_csv('first_bulk_of_routes_data.csv')
+
+#setting path back as it was before
+os.chdir(r'[substitude your path]\WayToGo\wt21-way-to-go\cleaning_data')
