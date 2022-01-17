35a823d8653c0f8f332abb455fbdfdc5c9d228a8
Sara Pulido <sara01rizo@gmail.com>
2022-01-17 06:26:42 +0100

    Update: navbar component link to items and rename logo's file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import logo from "..images/wtg.png";
+import logo from "./images/logowtg";
+import { navItems } from "./NavItems.js";
 
 function Navbar() {
   return (
@@ -9,6 +10,18 @@ function Navbar() {
         <Link to="/" className="navbar-logo">
           <img src={logo} alt="this is the logo" />
         </Link>
+        <ul className="nav-items">
+          {navItems.map((item) => {
+            return (
+              <li key={item.id} className={item.nName}>
+                <Link to={item.path}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       </nav>
     </div>
   );
