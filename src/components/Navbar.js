c635636a4df4e4f671c2d2c2f10e8b1bacc8aa91
Sara Pulido <sara01rizo@gmail.com>
2022-01-17 08:22:43 +0100

    Add: use Efect to const setMobile -mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "./images/wtg-logo-blue.png";
 import { navItems } from "./NavItems.js";
 import "./Navbar.css";
 
 function Navbar() {
+  const [mobile, setMobile] = useState(false);
+  const [sidebar, setSidebar] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth < 1065) {
+      setMobile(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 1065) {
+        setMobile(true);
+      } else {
+        setMobile(false);
+        setSidebar(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div>
       <nav className="navbar">
-        <Link to="/" className="navbar-logo">
-          <img src={logo} className="itemLogo" alt="this is the logo" />
+        <Link to="/" className="navbar-logo" onClick={() => setSidebar(false)}>
+          <img src={logo} className="itemLogo" alt="this is the logo" /> Logo
         </Link>
-        <ul className="nav-items">
-          {navItems.map((item) => {
-            return (
-              <li key={item.id} className={item.nName}>
-                <Link to={item.path}>
-                  {item.icon}
-                  <span>{item.title}</span>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
+        {!mobile && (
+          <ul className="nav-items">
+            {navItems.map((item) => {
+              return (
+                <li key={item.id} className={item.nName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+        {!mobile}
       </nav>
     </div>
   );
