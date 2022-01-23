1c80deac0346a2bf9a12f406555a3bd8c7824f66
Merve <merveb.duman@hotmail.com>
2022-01-23 13:49:35 +0100

    creating a sqlite db and edited gitignore

--- flask/create_sqlite_db.py
+++ flask/create_sqlite_db.py
@@ -1,17 +1,14 @@
-import sqlite3
-from sqlite3 import Error
+import csv, sqlite3
 
-def create_connection(db_file):
-    """ create a database connection to a SQLite database """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
+con = sqlite3.connect("/Users/mervebduman/wt21-way-to-go/flask/routes.db") # change to 'sqlite:///your_filename.db'
+cur = con.cursor()
+cur.execute("CREATE TABLE t (route_id, route_linestring, num_of_waypoint, poi_lat_lgt, poi_category, poi_name, distance);") # use your column names here
 
-if __name__ == '__main__':
-    create_connection(/Users/mervebduman/wt21-way-to-go/routes.db)
+with open('joint_data.csv','r') as fin: # `with` statement available in 2.5+
+    # csv.DictReader uses first line in file for column headings by default
+    dr = csv.DictReader(fin) # comma is default delimiter
+    to_db = [(i['route_id'], i['route_linestring'], i['num_of_waypoint,'], i['poi_lat_lgt'], i['poi_category'], i['poi_name'], i['distance']) for i in dr]
+
+cur.executemany("INSERT INTO t (route_id, route_linestring, num_of_waypoint, poi_lat_lgt, poi_category, poi_name, distance) VALUES (?, ?);", to_db)
+con.commit()
+con.close()
