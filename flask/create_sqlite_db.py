1f16122552791ec0c24ff1f8612c3bef88b435d4
Merve <merveb.duman@hotmail.com>
2022-01-23 14:26:19 +0100

    editing sqlite3 db

--- flask/create_sqlite_db.py
+++ flask/create_sqlite_db.py
@@ -1,14 +1,44 @@
-import csv, sqlite3
+# Import required modules
+import csv
+import sqlite3
 
-con = sqlite3.connect("/Users/mervebduman/wt21-way-to-go/flask/routes.db") # change to 'sqlite:///your_filename.db'
-cur = con.cursor()
-cur.execute("CREATE TABLE t (route_id, route_linestring, num_of_waypoint, poi_lat_lgt, poi_category, poi_name, distance);") # use your column names here
+# Connecting to the routes database
+connection = sqlite3.connect('routes.db')
 
-with open('joint_data.csv','r') as fin: # `with` statement available in 2.5+
-    # csv.DictReader uses first line in file for column headings by default
-    dr = csv.DictReader(fin) # comma is default delimiter
-    to_db = [(i['route_id'], i['route_linestring'], i['num_of_waypoint,'], i['poi_lat_lgt'], i['poi_category'], i['poi_name'], i['distance']) for i in dr]
+# Creating a cursor object to execute
+# SQL queries on a database table
+cursor = connection.cursor()
 
-cur.executemany("INSERT INTO t (route_id, route_linestring, num_of_waypoint, poi_lat_lgt, poi_category, poi_name, distance) VALUES (?, ?);", to_db)
-con.commit()
-con.close()
+# Table Definition
+create_table = '''CREATE TABLE routes(
+				route_id INTEGER,
+				route_linestring LONGTEXT,
+				num_of_waypoint INTEGER,
+				poi_lat_lgt LONGTEXT,
+				poi_category TEXT,
+				poi_name TEXT,
+				distance INTEGER);
+				'''
+
+# Creating the table into our database
+cursor.execute(create_table)
+
+# Opening the joint_data.csv file
+file = open('joint_data.csv')
+
+# Reading the contents of the joint_data.csv file
+contents = csv.reader(file)
+
+# SQL query to insert data into the person table
+insert_records = "INSERT INTO routes (route_id, route_linestring, num_of_waypoint, poi_lat_lgt, poi_category, poi_name, distance) VALUES(?, ?, ?, ?, ?, ?, ?)"
+
+# Importing the contents of the file into our routes table
+cursor.executemany(insert_records, contents)
+
+# SQL query to retrieve all data from the routes table to verify that the data of the csv file has been successfully inserted into the table
+select_all = "SELECT * FROM routes"
+rows = cursor.execute(select_all).fetchall()
+
+# closing the database connection
+connection.close()
+
