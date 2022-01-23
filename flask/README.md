4ecab1ba625d887b4e232bf00cab115241a8843e
Merve <merveb.duman@hotmail.com>
2022-01-23 17:36:54 +0100

    creating a sql db from pd and README.md

---
+++ flask/README.md
@@ -0,0 +1,19 @@
+#Flask
+##Activate the environment
+Before you work on your project, activate the corresponding environment **in the project directory**. 
+
+**for MacOS:**
+```shell
+$ . venv/bin/activate
+```
+**for Windows:**
+```shell
+> venv\Scripts\activates
+```
+
+Then install Flask, Flask0SQLAlchemy:
+
+```shell
+pip install Flask
+pip install Flask-SQLAlchemy
+```
--- flask/create_sqlite_db.py
+++ flask/create_sqlite_db.py
@@ -1,44 +1,11 @@
-# Import required modules
-import csv
-import sqlite3
+import pandas as pd
 
-# Connecting to the routes database
-connection = sqlite3.connect('routes.db')
+data = pd.read_csv ("joint_data.csv")   
+df = pd.DataFrame(data)
 
-# Creating a cursor object to execute
-# SQL queries on a database table
-cursor = connection.cursor()
+import flask
+from sqlalchemy import create_engine
+engine = create_engine('sqlite://', echo=False)
+df.to_sql('routes.db', con=engine)
 
-# Table Definition
-create_table = '''CREATE TABLE routes(
-				route_id INTEGER,
-				route_linestring LONGTEXT,
-				num_of_waypoint INTEGER,
-				poi_lat_lgt LONGTEXT,
-				poi_category TEXT,
-				poi_name TEXT,
-				distance INTEGER);
-				'''
-
-# Creating the table into our database
-cursor.execute(create_table)
-
-# Opening the joint_data.csv file
-file = open('joint_data.csv')
-
-# Reading the contents of the joint_data.csv file
-contents = csv.reader(file)
-
-# SQL query to insert data into the person table
-insert_records = "INSERT INTO routes (route_id, route_linestring, num_of_waypoint, poi_lat_lgt, poi_category, poi_name, distance) VALUES(?, ?, ?, ?, ?, ?, ?)"
-
-# Importing the contents of the file into our routes table
-cursor.executemany(insert_records, contents)
-
-# SQL query to retrieve all data from the routes table to verify that the data of the csv file has been successfully inserted into the table
-select_all = "SELECT * FROM routes"
-rows = cursor.execute(select_all).fetchall()
-
-# closing the database connection
-connection.close()
-
+#engine.execute("SELECT * FROM routes").fetchall()
