e29c0c5c949d99f49453f47dbf297a6691ccc95b
Merve <merveb.duman@hotmail.com>
2022-01-23 22:09:09 +0100

    Cleaned version of api.py

--- flask/api.py
+++ flask/api.py
@@ -18,7 +18,7 @@ app.register_blueprint(home_bp, url_prefix='/')
 # A route to return all of the available entries in our database.
 @app.route('/routes/all', methods=['GET'])
 def api_all():
-    conn = sqlite3.connect('/Users/mervebduman/wt21-way-to-go/flask/routes.db') # Add the routes db
+    conn = sqlite3.connect('routes.db') # To connect the database to the server.
     conn.row_factory = dict_factory
     cur = conn.cursor()
     all_routes = cur.execute('SELECT * FROM routes;').fetchall()
@@ -80,40 +80,5 @@ def api_filter():
     return jsonify(results)
 # Check the results here : http://127.0.0.1:5000/routes/?route_id=3543318
 # http://127.0.0.1:5000/routes/?poi_lat_lgt=POINT+(52.5498034+13.4141921)
-
-# Test data
-#routes = [
-#    {'name':'Berliner_Restaurant',
-#     'amenity':'restaurant',
-#     'lat':'47.52142',
-#     'lon':'10.49863'},
-#
-#    {'name':'Eiscafe',
-#     'amenity':'ice_cream',
-#     'lat': '50.9273',
-#     'lon': '8.47206'}
-#]
-
-# @app.route('/routes/', methods=['GET'])
-# def api_route_id():
-#     # Check if a route id was provided as part of the URL.
-#     # If route id is provided, assign it to a variable.
-#     # If no route id is provided, display an error in the browser.
-#     if 'route_id' in request.args:
-#         name = int(request.args['route_id']) # For numbers: 'int', for text 'str'
-#     else:
-#         return "Error: No route_id field provided. Please specify a route_id." # Accessing the link without providing a name will give the error message.
-#     # Create an empty list for our results
-#     results = []
-#
-#     # Loop through the data and match results that fit the request id.
-#     # Names may not be unique, thus they might return many results.
-#     for route in routes:
-#         if route['route_id'] == route_id:
-#             results.append(route)
-#
-#         # Use the jsonify function from Flask to convert our list of python dictionaries to the JSON format.
-#         return jsonify(results)
-
 
 app.run()
