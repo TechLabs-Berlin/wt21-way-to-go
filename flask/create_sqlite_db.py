27186c50f52a49c5b5d497460b267efb09334ddd
Merve <merveb.duman@hotmail.com>
2022-01-23 17:51:13 +0100

    Final working version of create_sqlite_db

--- flask/create_sqlite_db.py
+++ flask/create_sqlite_db.py
@@ -1,4 +1,11 @@
 import pandas as pd
+import sqlite3
+
+conn = sqlite3.connect('routes.db')
+c = conn.cursor()
+
+c.execute('CREATE TABLE routes (route_id, route_linestring, num_of_waypoint, poi_lat_lgt, poi_category, poi_name, distance)')
+conn.commit()
 
 data = pd.read_csv ("joint_data.csv")   
 df = pd.DataFrame(data)
@@ -6,6 +13,13 @@ df = pd.DataFrame(data)
 import flask
 from sqlalchemy import create_engine
 engine = create_engine('sqlite://', echo=False)
-df.to_sql('routes.db', con=engine)
+df.to_sql('routes.db', conn, if_exists='replace', index=False)
+
+c.execute('''SELECT * FROM routes''')
+
+for row in c.fetchall():
+    print(row)
 
 #engine.execute("SELECT * FROM routes").fetchall()
+
+#header = route_id, route_linestring, num_of_waypoint, poi_lat_lgt, poi_category, poi_name, distance
