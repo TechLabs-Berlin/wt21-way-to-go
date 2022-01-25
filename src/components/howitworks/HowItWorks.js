a869638ebf6cf5dc82494263f484858f4c598ebc
Sara Pulido <sara01rizo@gmail.com>
2022-01-26 00:28:49 +0100

    Build: section hiw -lines images rotate -ready

--- src/components/howitworks/HowItWorks.js
+++ src/components/howitworks/HowItWorks.js
@@ -3,16 +3,19 @@ import "../howitworks/HowItWorks.css";
 
 import { Container, Row, Col, Image } from "react-bootstrap";
 import demo from "../images/demo-hiw.jpg";
+import line from "../images/line-wtg-1-1.png";
+import line2 from "../images/line-wtg-2-2.png";
+import line3 from "../images/line-wtg-3-3.png";
+import line4 from "../images/line-wtg-4-4.png";
 //Row = 12 cols or 100%
 
 function HowItWorks() {
   return (
-    <div>
+    <div className="hiw_section_landing">
       <h2>
         <b>HOW IT WORKS!</b>
       </h2>
-
-      <Container>
+      <Container fluid>
         <Row className="text-center">
           <Col md={4} sm={6}>
             <figure>
@@ -20,14 +23,15 @@ function HowItWorks() {
             </figure>
           </Col>
           <Col md={4} sm={6}>
-            Hello second colum LINES
+            <figure>
+              <Image src={line} alt="cool line demo" fluid className="line1" />
+            </figure>
           </Col>
           <Col md={4} sm={6}>
             <b>#1 Set starting point and a destination</b>
             <p>What are the two places you want to spend time in-between?</p>
           </Col>
         </Row>
-
         <Row className="text-center">
           <Col md={4} sm={6}>
             <b className="title-steps">#2 Choose your mood</b>
@@ -38,7 +42,9 @@ function HowItWorks() {
             </p>
           </Col>
           <Col md={4} sm={6}>
-            Hello second colum LINES
+            <figure>
+              <Image src={line2} alt="cool line demo" fluid className="line2" />
+            </figure>
           </Col>
           <Col md={4} sm={6}>
             <figure>
@@ -46,7 +52,6 @@ function HowItWorks() {
             </figure>
           </Col>
         </Row>
-
         <Row className="text-center">
           <Col md={4} sm={6}>
             <figure>
@@ -54,7 +59,9 @@ function HowItWorks() {
             </figure>
           </Col>
           <Col md={4} sm={6}>
-            Hello second colum LINES
+            <figure>
+              <Image src={line3} alt="cool line demo" fluid className="line3" />
+            </figure>
           </Col>
           <Col md={4} sm={6}>
             <b>#3 Arrange time duration</b>
@@ -63,12 +70,15 @@ function HowItWorks() {
             </p>
           </Col>
         </Row>
+        <section className="enjoy_link">
+          <p className="bottom_side">
+            <b>Enjoy your WAY TO GO</b>
+          </p>
+        </section>
+        <figure>
+          <Image src={line4} alt="cool line demo" fluid className="line4" />
+        </figure>
       </Container>
-      <section className="enjoy_link">
-        <p className="bottom_side">
-          <b>Enjoy your WAY TO GO</b>
-        </p>
-      </section>
     </div>
   );
 }
