41546c77e9dad76e933ee0bce32498bfd489b7b0
Sara Pulido <sara01rizo@gmail.com>
2022-01-28 21:36:14 +0100

    Update: background and text figcaption

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,27 @@
 import React from "react";
+import "./About.css";
+import BackgroundAbout from "../images/alexander1.jpeg";
 
 function About() {
   return (
     <div>
-      <h1 className="About">Contact Us</h1>
+      <div className="container">
+        <figure className="position-relative">
+          <img
+            className="BackgroundAbout"
+            src={BackgroundAbout}
+            alt="Alexander Platz"
+          />
+          <figcaption>
+            {" "}
+            <h1 className="about">About</h1>
+            <p>
+              WayToGo provides personalized map service, <br></br> that covers
+              400+ popular routes in Berlin
+            </p>
+          </figcaption>
+        </figure>
+      </div>
     </div>
   );
 }
