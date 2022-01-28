e5bb1480a1b46c9e075d4b08492ac719a268f192
Leonel Acosta <leoacosta88@gmail.com>
2022-01-28 20:43:57 +0100

    Contact Page Template
    No styling applied

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,9 +1,33 @@
 import React from "react";
+import "./Contact.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { Container, Row, Figure} from "react-bootstrap";
+
 
 function Contact() {
   return (
-    <div>
-      <h1 className="contact">Contact Us</h1>
+    <div className="contact-page">
+      <div className="contact-hero">
+      <Container>
+        <Row>
+      <h2><strong>Contact Us</strong></h2>
+      <h3>Drop us a line! We would love to hear from you</h3>
+      </Row>
+      </ Container>
+      </div>
+
+      <div className="contact-info">
+<Container>
+      <Row>
+<span>WayToGo GMBH</span>
+<span>MustermannStr .37</span>
+<span>10437 Berlin</span>
+<br/>
+<br/>
+      <span><a class="link" href="mailto:hallo@waytogo.com">Hello@waytogo.com</a></span>
+      </Row>
+      </ Container>
+      </div>
     </div>
   );
 }
