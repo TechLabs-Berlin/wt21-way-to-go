2d4918fd774e57f304d4e75aee7ed6687ca87e5b
Sara Pulido <sara01rizo@gmail.com>
2022-02-01 10:49:46 +0100

    Remove: code that appears double after pull from main

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,8 @@ import SearchFormContainer from "../components/search/searchFormContainer";
 import Map from "../components/map/map";
 import HowItWorks from "./../components/howitworks/HowItWorks";
 import CardsHome from "../components/cardsHome/CardsHome";
-import BackgroundImage from "../images/landingBackground.jpeg"
-import "./Home.css"
+import BackgroundImage from "../images/landingBackground.jpeg";
+import "./Home.css";
 import MapLoadMenu from "../components/map/mapLoadMenu";
 
 const libraries = ["places"];
@@ -28,46 +28,40 @@ function Home() {
 
   return (
     <div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
       {!routeResponse && (
         <div>
+          <div className="homeText">
+            <p>WELCOME</p>
+            <p>TO</p>
+            <p>BERLIN</p>
+          </div>
           <SearchFormContainer
             routeResponse={routeResponse}
             setRouteResponse={setRouteResponse}
+            setFrom={setFrom}
+            setTo={setTo}
+            from={from}
+            to={to}
+          />
+          <img
+            className="backgroundHomeImage"
+            src={BackgroundImage}
+            alt="berlinImage"
           />
           <CardsHome expand="md" />
           <HowItWorks expand="md" />
         </div>
       )}
->>>>>>> main
-=======
->>>>>>> main
-      {!routeResponse && <div>
-        <div className="homeText">
-          <p>WELCOME</p>
-          <p>TO</p>
-          <p>BERLIN</p>
-        </div>
-        <SearchFormContainer
-          routeResponse={routeResponse}
-          setRouteResponse={setRouteResponse}
-          setFrom={setFrom}
-          setTo={setTo}
-          from={from}
-          to={to}
-        />
-        <img className="backgroundHomeImage" src={BackgroundImage} alt="berlinImage" />
-        <CardsHome expand="md" />
-        <HowItWorks expand="md" />
-      </div>
-      }
 
-      {isLoaded && routeResponse && (<div>
-        <MapLoadMenu from={from} to={to} setRouteResponse={setRouteResponse} />
-        <Map onMapLoad={onMapLoad} routeResponse={routeResponse} />
-      </div>
+      {isLoaded && routeResponse && (
+        <div>
+          <MapLoadMenu
+            from={from}
+            to={to}
+            setRouteResponse={setRouteResponse}
+          />
+          <Map onMapLoad={onMapLoad} routeResponse={routeResponse} />
+        </div>
       )}
     </div>
   );
