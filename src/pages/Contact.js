32d832b231fd93b410d2005407ac234a5d4954a4
leonelacosta <leoacosta88@gmail.com>
2022-02-01 20:35:23 +0100

    Remove styling and contact page from branch

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,38 +1,9 @@
 import React from "react";
-import "./Contact.css";
-import "bootstrap/dist/css/bootstrap.css";
-import BackgroundContact from "../images/contact-background.jpeg";
 
 function Contact() {
   return (
-    <div className="container">
-      <figure className="position-relative">
-        <img
-          className="BackgroundContact"
-          src={BackgroundContact}
-          alt="Contact Us"
-        />
-        <figcaption>
-          {" "}
-          <h1>Contact Us</h1>
-          <p>Drop us a line! We would love to hear from you</p>
-        </figcaption>
-      </figure>
-
-      <div className="contact-info">
-        <p className="contact-text">
-          <span>WayToGo GMBH</span>
-          <span>MustermannStr .37</span>
-          <span>10437 Berlin</span>
-          <br />
-          <br />
-          <span>
-            <a class="link" href="mailto:hallo@waytogo.com">
-              Hello@waytogo.com
-            </a>
-          </span>
-        </p>
-      </div>
+    <div>
+      <h1 className="contact">Contact Us</h1>
     </div>
   );
 }
