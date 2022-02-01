d22a79cfb8735c29ec65b958ed829929ab6c16df
Leonor Cascais <91019129+cleonor@users.noreply.github.com>
2022-02-01 19:20:42 +0100

    Revert "Footer / Sticky footer / Contact Page"

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navBar/Navbar";
-import Footer from "./components/footer/footer";
-import StickyFooter from "./components/stickyFooter/stickyfooter";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
@@ -21,9 +19,6 @@ const App = function () {
           <Route path="/contact" element={<Contact />}></Route>
         </Routes>
       </BrowserRouter>
-      <Footer />
-      <StickyFooter />
-
     </div>
   );
 };
