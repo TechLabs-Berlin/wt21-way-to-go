52587d453973f5795217df240d3ded932c18efc7
Sara Pulido <sara01rizo@gmail.com>
2022-02-03 14:10:12 +0100

    Last minute: sweetAlert2 and link inside how page - done

--- src/pages/How.js
+++ src/pages/How.js
@@ -6,21 +6,31 @@ function How() {
   const showAlert = () => {
     new swal({
       title: "How it Works 🌏",
-      text: "Go and click the next button 🐱‍🏍",
+      text: "Go and click the link 🐱‍🏍",
       icon: "success",
       button: "Agree",
       timer: "7000",
       footer: "TechLabs Rocks!!",
+      timerProgressBar: true,
     });
   };
+
   return (
     <div>
+      <br></br>
       <button
-        className="mx-auto d-block btn-outline-danger-how"
-        onClick={() => showAlert()}
+        className="mx-auto d-block btn-outline-info-how2"
+        onClick={() => showAlert`<a href="./Home.js"></a>`}
       >
         🗺 Click me!
       </button>
+
+      <br></br>
+      <section className="enjoy_link_how">
+        <a href="http://localhost:3000/" className="link_go_home">
+          Enjoy your WAY TO GO
+        </a>
+      </section>
     </div>
   );
 }
