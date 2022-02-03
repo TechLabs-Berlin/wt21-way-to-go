234841aa01a6603a4b699af9c08d9349495588a5
Sara Pulido <sara01rizo@gmail.com>
2022-02-03 12:06:30 +0100

    Add: SweetAlert2 to how it works page

--- src/pages/How.js
+++ src/pages/How.js
@@ -1,9 +1,26 @@
 import React from "react";
+import "./How.css";
+import swal from "sweetalert2";
 
 function How() {
+  const showAlert = () => {
+    new swal({
+      title: "How it Works 🌏",
+      text: "Go and click the next button 🐱‍🏍",
+      icon: "success",
+      button: "Agree",
+      timer: "7000",
+      footer: "TechLabs Rocks!!",
+    });
+  };
   return (
     <div>
-      <h1 className="How">How</h1>
+      <button
+        className="mx-auto d-block btn-outline-danger-how"
+        onClick={() => showAlert()}
+      >
+        🗺 Click me!
+      </button>
     </div>
   );
 }
